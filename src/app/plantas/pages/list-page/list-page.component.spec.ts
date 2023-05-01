18c53b92928182e96ed95d773d28ededb7889da3
Herald Olivares <heraldmatias.oz@gmail.com>
2023-04-30 19:49:12 -0500

    Adding unit tests

--- src/app/plantas/pages/list-page/list-page.component.spec.ts
+++ src/app/plantas/pages/list-page/list-page.component.spec.ts
@@ -1,14 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListPageComponent } from './list-page.component';
+import { PlantasService } from '../../services/plantas.service';
+import { Observable, of } from 'rxjs';
+import { Planta } from '../../interfaces/plantas.interface';
 
 describe('ListPageComponent', () => {
   let component: ListPageComponent;
   let fixture: ComponentFixture<ListPageComponent>;
+  let mockPlantasService: jasmine.SpyObj<PlantasService>;
 
   beforeEach(async () => {
+    mockPlantasService = jasmine.createSpyObj('PlantasService', ['getPlantas']);
+    const testData: Planta[] = [
+      {
+        "id": 1,
+        "nombre_comun": "Lengua de vaca",
+        "nombre_cientifico": "Sansevieria Trifasciata",
+        "tipo": "Interior",
+        "altura_maxima": 140,
+        "clima": "Templado, cálido",
+        "sustrato_siembra": "Tierra orgánica con buen drenaje, arena, cascarilla de arroz"
+    },
+    {
+        "id": 2,
+        "nombre_comun": "Chachafruto",
+        "nombre_cientifico": "Schefflera actinophylla",
+        "tipo": "Exterior",
+        "altura_maxima": 1000,
+        "clima": "Todos",
+        "sustrato_siembra": "Sustrato para huerto"
+    },
+    {
+        "id": 3,
+        "nombre_comun": "Espatifilo",
+        "nombre_cientifico": "Spathiphyllum Wallasii",
+        "tipo": "Interior",
+        "altura_maxima": 65,
+        "clima": "Templado, cálido",
+        "sustrato_siembra": "Tierra orgánica"
+    },
+    ]
+    mockPlantasService.getPlantas.and.returnValue( of(testData));
+
     await TestBed.configureTestingModule({
-      declarations: [ ListPageComponent ]
+      declarations: [ ListPageComponent ],
+      providers: [{ provide: PlantasService, useValue: mockPlantasService }],
     })
     .compileComponents();
 
